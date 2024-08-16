9dba8da9b7d8bf1cf81596f6c58bb49489e516fc
po3rdlp <leonpolandos53@gmail.com>
2024-08-16 12:57:30 +0800

    add global prefix

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+  
+  app.setGlobalPrefix('dev/v1')
+
   await app.listen(3500);
 }
 bootstrap();
